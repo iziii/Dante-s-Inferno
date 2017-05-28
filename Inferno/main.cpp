cdce07e144b08f8abfbd3b6c534927b98186eb11
LAPTOP-LI0S1L29\IZI <paulaiz876@gmail.com>
2017-05-29 01:14:47 +0200

    update.

--- Inferno/main.cpp
+++ Inferno/main.cpp
@@ -6,7 +6,8 @@ int main(int argc, char const *argv[])
   Names::init();
 
   int amount = 5; //liczba grzesznikow
-  short years, max=0;
+  int numberOfAlive = amount;
+  short year = 0;
 
   int cricle_1, circle_2, circle_3, circle_4; //zmienne odpowiadajace za poziomy w piekle
 
@@ -16,29 +17,23 @@ int main(int argc, char const *argv[])
 std::vector <Human>::iterator it_people;
 std::vector <Sin>::iterator it_sins;
 
-for(size_t i=0; i<people.size(); i++)
+while (numberOfAlive > 0)
 {
-  max = std::max(people[i].lifetime(), max);
-}
-
-  years = max;
-
-for(int i=0; i<max; i++)
-{
-    for(it_people = people.begin(); it_people!=people.end(); it_people++)
+  for(it_people = people.begin(); it_people!=people.end(); it_people++)
+  {
+    if(it_people -> isDead() != true)
     {
-      if(it_people -> isDead() != true)
+      for(size_t i=0; i < sins.size(); i++)
+        it_people -> commitSins(&sins[i]);
+
+      if(it_people -> lifetime() == year )
       {
-        for(size_t k=0; k<sins.size(); k++)
-          it_people -> commitSins(&sins[k]);
-
-        if(it_people -> lifetime() == i )
           it_people -> die();
-
-
-        }
-
+          numberOfAlive--;
+      }
     }
+  }
+  year++;
 }
 
 //TU NA RAZIE TYLKO TESTY CZY DOBRZE I JAK GENERUJA SIE LUDZIE, ITP.
@@ -73,10 +68,6 @@ for(it_people=people.begin(); it_people!=people.end(); it_people++)
   printf("Zyl: %i lat\n\n", it_people ->lifetime() );
 
   printf("Liczba popelnionych grzechow ogolem: %i\n\n", it_people ->numberOfAllSins());
-  printf("Liczba popelnionych grzechow z kregu I: %i\n", it_people ->numberOfSins(1));
-  printf("Liczba popelnionych grzechow z kregu II: %i\n", it_people ->numberOfSins(2));
-  printf("Liczba popelnionych grzechow z kregu III: %i\n", it_people ->numberOfSins(3));
-  printf("Liczba popelnionych grzechow z kregu IV: %i\n\n", it_people ->numberOfSins(4));
 
   printf("--------------- \n");
 
